322466329e24674bf85d4563cc9df407b6357929
SleepyFish <151081838+SleepyFish-YT@users.noreply.github.com>
2024-02-15 06:13:01 +0100

    Add files via upload

---
+++ me.sleepy.example/Example.cpp
@@ -0,0 +1,15 @@
+#include "Example.h"
+
+#include <iostream>
+#include "windows.h"
+
+FILE* file;
+void Example::Inject() {
+
+    AllocConsole();
+    SetConsoleTitle("Example by SleepyFish");
+    freopen_s(&file, "CONOUT$", "w", stdout);
+
+    printf(" Your client name or what ever ? \n");
+
+}
